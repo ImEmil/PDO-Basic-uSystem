f01bb318190b505af18dacf6f64385740d67c035
ImEmil <emil_nilsson_96@live.se>
2013-12-31 23:23:24 +0100

    The important sql file you need to execute in PhpMyAdmin/NaviCat

---
+++ db_table.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS `github` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `github`;
+
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `to` text NOT NULL,
+  `from` text NOT NULL,
+  `comment` text NOT NULL,
+  `time` varchar(150) NOT NULL,
+  `ip` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `rank` varchar(10) NOT NULL,
+  `ip` text NOT NULL,
+  `email` text NOT NULL,
+  `lastlogin` text NOT NULL,
+  `joindate` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
